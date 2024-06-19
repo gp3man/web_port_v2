43a5aa38ee504b5317bf7f5451166b33ee1253b9
Greg <greg.pounds3@gmail.com>
2024-06-19 06:56:01 -0500

    creating crown

---
+++ app/components/BentoMain.tsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import { BentoGrid } from './ui/bento-grid'
+import { BentoGridItem } from './ui/bento-grid'
+import { PaperPlaneIcon } from '@radix-ui/react-icons'
+import { Skeleton } from './ui/skeleton';
+
+const BentoMain = () => {
+  const items = [
+    {
+      title: "The Dawn of Innovation",
+      description: "Explore the birth of groundbreaking ideas and inventions.",
+      header: <Skeleton />,
+      icon: <PaperPlaneIcon className="h-4 w-4 text-neutral-500" />,
+    },
+    {
+      title: "The Digital Revolution",
+      description: "Dive into the transformative power of technology.",
+      header: <Skeleton />,
+      icon: <PaperPlaneIcon className="h-4 w-4 text-neutral-500" />,
+    },
+    {
+      title: "The Art of Design",
+      description: "Discover the beauty of thoughtful and functional design.",
+      header: <Skeleton />,
+      icon: <PaperPlaneIcon className="h-4 w-4 text-neutral-500" />,
+    },
+    {
+      title: "The Power of Communication",
+      description:
+        "Understand the impact of effective communication in our lives.",
+      header: <Skeleton />,
+      icon: <PaperPlaneIcon className="h-4 w-4 text-neutral-500" />,
+    },
+    {
+      title: "The Pursuit of Knowledge",
+      description: "Join the quest for understanding and enlightenment.",
+      header: <Skeleton />,
+      icon: <PaperPlaneIcon className="h-4 w-4 text-neutral-500" />,
+    },
+    {
+      title: "The Joy of Creation",
+      description: "Experience the thrill of bringing ideas to life.",
+      header: <Skeleton />,
+      icon: <PaperPlaneIcon className="h-4 w-4 text-neutral-500" />,
+    },
+    {
+      title: "The Spirit of Adventure",
+      description: "Embark on exciting journeys and thrilling discoveries.",
+      header: <Skeleton />,
+      icon: <PaperPlaneIcon className="h-4 w-4 text-neutral-500" />,
+    },
+  ]
+
+  return (
+    <div>BentoMain
+      <BentoGrid className="max-w-4xl mx-auto">
+      {items.map((item, i) => (
+        <BentoGridItem
+          key={i}
+          title={item.title}
+          description={item.description}
+          header={item.header}
+          icon={item.icon}
+          className={i === 3 || i === 6 ? "md:col-span-2" : ""}
+        />
+      ))}
+    </BentoGrid>
+    </div>
+  )
+}
+
+export default BentoMain
